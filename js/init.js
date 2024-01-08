fdd3ca320b0a3376cadd4586d77a81510d2f7b74
Madeleine Beckner <40649770+mebeckner@users.noreply.github.com>
2024-01-07 20:10:51 -0700

    add js

--- js/init.js
+++ js/init.js
@@ -4,21 +4,6 @@
     Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
 */
 
-let slideIndex = 0;
-showSlides();
-
-function showSlides() {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
-}
-
 skel.init({
 	prefix: 'css/style',
 	resetCSS: true,
